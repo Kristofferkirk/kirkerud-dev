6655e0e2f09fd89eeeb97e6d33d4fa71b0ec4da0
Kristoffer Kirkerud <kristofferkirkerud@outlook.com>
2021-07-24 14:14:47 +0200

    Added clean landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import ScrollableSection from "../components/ScrollableSection";
 import styles from "../styles/Home.module.css";
-
+import githublink from "../public/github.png";
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -16,66 +16,47 @@ export default function Home() {
         <h1 className={styles.title}>Kristoffer Kirkerud</h1>
 
         <p className={styles.description}>Fullstack developer</p>
-
-        <div className={styles.gridFull}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in
-          magna id tellus efficitur placerat non vitae nulla. Cras accumsan,
-          nibh nec dignissim lobortis, felis quam volutpat diam, id cursus magna
-          ante vitae mauris. Nullam suscipit aliquet lobortis. Maecenas lobortis
-          turpis lorem, id sodales nisi dictum eu. Nunc condimentum dolor sem,
-          ut mattis ex facilisis eget. Vestibulum molestie massa arcu, vitae
-          varius lectus mollis vel. Sed augue lacus, suscipit aliquet
-          sollicitudin eu, rutrum varius lacus. Donec rhoncus sapien pulvinar mi
-          egestas tempor. Nam rutrum dolor eu quam posuere, quis ultricies massa
-          rutrum. Morbi vel faucibus orci. Donec placerat, sem nec sodales
-          rutrum, enim mi convallis turpis, vel porta sapien quam sed leo. Duis
-          elementum eget lacus lacinia eleifend. Nullam ac elit tortor. Integer
-          feugiat nunc dictum enim laoreet, id convallis lectus commodo.
-          Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-          posuere cubilia curae;
-        </div>
-        <div className={styles.gridFull}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in
-          magna id tellus efficitur placerat non vitae nulla. Cras accumsan,
-          nibh nec dignissim lobortis, felis quam volutpat diam, id cursus magna
-          ante vitae mauris. Nullam suscipit aliquet lobortis. Maecenas lobortis
-          turpis lorem, id sodales nisi dictum eu. Nunc condimentum dolor sem,
-          ut mattis ex facilisis eget. Vestibulum molestie massa arcu, vitae
-          varius lectus mollis vel. Sed augue lacus, suscipit aliquet
-          sollicitudin eu, rutrum varius lacus. Donec rhoncus sapien pulvinar mi
-          egestas tempor. Nam rutrum dolor eu quam posuere, quis ultricies massa
-          rutrum. Morbi vel faucibus orci. Donec placerat, sem nec sodales
-          rutrum, enim mi convallis turpis, vel porta sapien quam sed leo. Duis
-          elementum eget lacus lacinia eleifend. Nullam ac elit tortor. Integer
-          feugiat nunc dictum enim laoreet, id convallis lectus commodo.
-          Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-          posuere cubilia curae;
-        </div>
-        <div className={styles.gridFull}>
-          <ScrollableSection>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in
-              magna id tellus efficitur placerat non vitae nulla. Cras accumsan,
-              nibh nec dignissim lobortis, felis quam volutpat diam, id cursus
-              magna ante vitae mauris. Nullam suscipit aliquet lobortis.
-              Maecenas lobortis turpis lorem, id sodales nisi dictum eu. Nunc
-              condimentum dolor sem, ut mattis ex facilisis eget. Vestibulum
-              molestie massa arcu, vitae varius lectus mollis vel. Sed augue
-              lacus, suscipit aliquet sollicitudin eu, rutrum varius lacus.
-              Donec rhoncus sapien pulvinar mi egestas tempor. Nam rutrum dolor
-              eu quam posuere, quis ultricies massa rutrum. Morbi vel faucibus
-              orci. Donec placerat, sem nec sodales rutrum, enim mi convallis
-              turpis, vel porta sapien quam sed leo. Duis elementum eget lacus
-              lacinia eleifend. Nullam ac elit tortor. Integer feugiat nunc
-              dictum enim laoreet, id convallis lectus commodo. Vestibulum ante
-              ipsum primis in faucibus orci luctus et ultrices posuere cubilia
-              curae;
-            </p>
-          </ScrollableSection>
+        <div className={styles.grid}>
+          <div className={styles.card}>
+            <strong>Frontend</strong>
+            <ul>
+              <li>React</li>
+              <li>Angular</li>
+              <li>Vue</li>
+            </ul>
+          </div>
+          <div className={styles.card}>
+            <strong>Backend</strong>
+            <ul>
+              <li>C#</li>
+              <li>Python</li>
+              <li>Java</li>
+            </ul>
+          </div>
+          <div className={styles.card}>
+            <strong>Cloud</strong>
+            <ul>
+              <li>Sharepoint</li>
+              <li>Azure</li>
+              <li>Google Domains</li>
+            </ul>
+          </div>
+          <div className={styles.card}>
+            <strong>Tools</strong>
+            <ul>
+              <li>Docker</li>
+              <li>VSCode</li>
+              <li>Photoshop</li>
+            </ul>
+          </div>
         </div>
       </main>
 
-      <footer className={styles.footer}></footer>
+      <footer className={styles.footer}>
+        <a href="https://github.com/Kristofferkirk">
+          <Image width={50} height={50} alt="github" src={githublink} />
+        </a>
+      </footer>
     </div>
   );
 }
